1d5a21faaac70cba5b69f0beebf3dc403b35e5b2
VineetPaun <129723487+VineetPaun@users.noreply.github.com>
2025-02-14 09:34:06 +0530

    created navbar

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+import { ThemeProvider } from '@/components/theme-provider'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <Navbar/>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
